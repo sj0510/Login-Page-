05fff547782a78bda81941e44c97e7df39ed986e
saurabh <saurabhjaiswal932@gmail.com>
2021-01-31 20:23:16 +0530

    first commit

---
+++ app/src/main/java/com/example/phoneauthlogin/VerifyPhoneActivity.java
@@ -0,0 +1,147 @@
+package com.example.phoneauthlogin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import static android.view.View.*;
+
+public class VerifyPhoneActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button BTN_VerifyOTP;
+    private EditText etOTPCode;
+    ProgressBar progressBar;
+
+    String phoneNumber;
+    String verificationId;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+
+        BTN_VerifyOTP = findViewById(R.id.btn_SignIn);
+        etOTPCode = findViewById(R.id.etOTPCode);
+        progressBar = findViewById(R.id.progressbar);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        phoneNumber = getIntent().getStringExtra("mobile");
+
+        //initiate OTP
+        initiateOTP(phoneNumber);
+
+        // save phone number
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("USER_PREF",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("phoneNumber", phoneNumber);
+        editor.apply();
+
+
+
+
+        BTN_VerifyOTP.setOnClickListener(this);
+
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(VerifyPhoneActivity.this, Profile.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(VerifyPhoneActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(GONE);
+                        }
+                    }
+                });
+    }
+
+    private void initiateOTP(String number) {
+        progressBar.setVisibility(VISIBLE);
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(number)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+        progressBar.setVisibility(GONE);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                etOTPCode.setText(code);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(VerifyPhoneActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(GONE);
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        String code = etOTPCode.getText().toString().trim();
+
+        if (code.isEmpty() || code.length() < 6) {
+
+            etOTPCode.setError("Enter OTP code...");
+            etOTPCode.requestFocus();
+            return;
+        }
+        verifyCode(code);
+    }
+}
